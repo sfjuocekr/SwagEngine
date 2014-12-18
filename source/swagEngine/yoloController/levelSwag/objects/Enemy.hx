529b3ac71fbdccdd9ef60147be775b2a8f63e360
Sjoer van der Ploeg <sfjuocekr@gmail.com>
2014-12-18 16:11:12 +0100

    Forgot some stuffs

---
+++ source/swagEngine/yoloController/levelSwag/objects/Enemy.hx
@@ -0,0 +1,20 @@
+package swagEngine.yoloController.levelSwag.objects;
+
+import nape.dynamics.InteractionFilter;
+import nape.phys.BodyType;
+import flixel.addons.nape.FlxNapeSprite;
+
+/**
+ * ...
+ * @author ...
+ */
+
+class Enemy extends FlxNapeSprite
+{
+
+	public function new() 
+	{
+		super();
+	}
+	
+}
