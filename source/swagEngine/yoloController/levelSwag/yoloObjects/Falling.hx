4eb5bbaeacefac07373c9e550032bf92ed778785
sfjuocekr <sfjuocekr@gmail.com>
2015-01-18 16:52:11 +0100

    Tagor
    Added classes

---
+++ source/swagEngine/yoloController/levelSwag/yoloObjects/Falling.hx
@@ -0,0 +1,16 @@
+package swagEngine.yoloController.levelSwag.yoloObjects;
+
+import flixel.FlxSprite;
+/**
+* ...
+* @author Sjoer van der Ploeg
+*/
+class Falling extends FlxSprite
+{
+
+	public function new() 
+	{
+		immovable = false;
+	}
+	
+}
---
+++ source/swagEngine/yoloController/levelSwag/yoloObjects/Invisible.hx
@@ -0,0 +1,18 @@
+package swagEngine.yoloController.levelSwag.yoloObjects;
+
+import flixel.FlxSprite;
+
+/**
+* ...
+* @author Sjoer van der Ploeg
+*/
+
+class Invisible extends FlxSprite
+{
+
+	public function new() 
+	{
+		//turns visible when button pressed
+	}
+	
+}
