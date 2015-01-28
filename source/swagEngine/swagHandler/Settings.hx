db14c6c7b2e7bf6e895a0773b660f685fafc594d
SophW <sophie.walker@hotmail.ch>
2015-01-28 22:37:48 +0100

    everything
    added pictures and sound to the intro

--- source/swagEngine/swagHandler/Settings.hx
+++ source/swagEngine/swagHandler/Settings.hx
@@ -15,7 +15,7 @@ class Settings
 	public static var levels:Array<Int> = [0, 1];
 	public static var level:Int = 0;
 	public static var saves:Array<FlxSave> = new Array();
-	public static var fullscreen:Bool = true;
+	public static var fullscreen:Bool = false;
 	public static var maxVelocity = 250;
 	public static var acceleration = 1000;
 	public static var drag = 1000;
